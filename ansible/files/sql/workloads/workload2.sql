99cfd425e5d16febb654b2d039cc2c8285b948ed
aksenov.andrey9 <aksenov.andrey9@wb.ru>
2024-01-21 16:23:03 +0700

    pg_hba config

---
+++ ansible/files/sql/workloads/workload2.sql
@@ -0,0 +1,7 @@
+INSERT INTO book.tickets (fkRide, fio, contact, fkSeat)
+VALUES (
+	ceil(random()*100)
+	, (array(SELECT fam FROM book.fam))[ceil(random()*110)]::text || ' ' ||
+    (array(SELECT nam FROM book.nam))[ceil(random()*110)]::text
+    ,('{"phone":"+7' || (1000000000::bigint + floor(random()*9000000000)::bigint)::text || '"}')::jsonb
+    , ceil(random()*100));
